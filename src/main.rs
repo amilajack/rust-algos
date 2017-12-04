b6c94f87077579c768d0013aee5fe5113b52dcb2
Amila Welihinda <amilajack@gmail.com>
2017-12-03 17:11:52 -0800

    Ignored targed from source control

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::collections::HashSet;
+
+fn main() {
+    let mut vec = vec!(1, 2, 3, 4);
+    for (i, item) in vec.iter() {
+        // println!("{}", i)
+        // vec[i] = vec[i] + 1;
+    }
+}
