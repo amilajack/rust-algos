df31c60f588333fb364a5487556058c1890efd07
Amila Welihinda <amilajack@gmail.com>
2018-01-31 14:27:09 -0800

    Added docs for math.rs

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,6 @@
+/// Find the greatest commond divisor of two integers
+/// a and b > 0. This is done in O(log(n)) time, where
+/// n is the number of digits of the larger of a or b
 pub fn euclidean_gcd(a: u32, b: u32) -> u32 {
     let (larger_number, smaller_number) = if a >= b {
         (a, b)
@@ -23,5 +26,7 @@ mod test {
     pub fn test_euclidea_gcd() {
         assert_eq!(euclidean_gcd(10, 5), 5);
         assert_eq!(euclidean_gcd(5, 15), 5);
+        assert_eq!(euclidean_gcd(3, 15), 3);
+        assert_eq!(euclidean_gcd(7, 11), 1);
     }
 }
